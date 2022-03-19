6567077c781291a28cf20e975f810cffe34bac12
Nicolas Vintrob <nicovintrob@gmail.com>
2022-03-19 12:05:10 -0300

    NavBar

--- app-vintrob/src/App.js
+++ app-vintrob/src/App.js
@@ -1,11 +1,13 @@
 /* import logo from './logo.svg'; */
 /* import './App.css'; */
 import Avatar from '../src/components/Components /Avatar'
+import NavBar from './components/NavBar/NavBar';
 
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" />
 
---
+++ app-vintrob/src/components/NavBar/NavBar.js
@@ -0,0 +1,11 @@
+const NavBar = () => {
+    return (
+        <nav>
+            <button>Inicio</button>
+            <button>Categorias</button>
+            <button>Nosotros</button>
+        </nav>
+    )
+}
+
+export default NavBar
