3f0829db1d973fa6b5108bd644c10c90bf5ba5b4
Nicolas Vintrob <nicovintrob@gmail.com>
2022-03-26 18:54:05 -0300

    Counter

--- app-vintrob/src/App.js
+++ app-vintrob/src/App.js
@@ -1,15 +1,22 @@
 /* import logo from './logo.svg'; */
 /* import './App.css'; */
 import Avatar from '../src/components/Components /Avatar'
+import ItemCount from './components/ItemCount/ItemCount';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import NavBar from './components/NavBar/NavBar';
 
 
 function App() {
+  
+  const onAdd = (quantity) => {
+    console.log(quantity)
+  }
+
   return (
     <div className="App">
       <NavBar />
       <ItemListContainer greeting="Gracias por elegirnos" />
+      <ItemCount initial={1} stock={10} onAdd={onAdd} />
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" />
 
